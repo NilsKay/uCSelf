c741aa66dbc4c0125caee8c34b5d75af8b881b64
nik <Nils.Kay@esko.com>
2025-11-24 16:33:07 +0100

    Some refactoring

--- src/SOUND/Project2.java
+++ src/SOUND/Project2.java
@@ -671,136 +671,15 @@ public class Project2 {
 	    	f_cnt++;
 	    else 
 	    	f_cnt = 0; // reset hit counter
-	    
-	    boolean addToLoop = false;
-	    if (rt.stateCnt <= 0) {
-	    	// update the loop
-	    	Note tn = new Note(start, 0, 0, 0, 0, 0, 0, 0, false);
-		    tn.note = freq;
-	    	if (loop.size() < def.loopDepth)
-	    		loop.addElement(tn);
-	    	else { // cache
-	    		loop.removeElementAt(0);
-	    		loop.addElement(tn);
-	    	}
-	    	//addLog("Add Loop-element at it="+it+" size now "+loop.size());
-	    	addToLoop = true;
-	    }
-	    
-	    Note loopNote  = null; 
-	    if (f_cnt >= def.trigger) { // Trigger the state machine, execute it at the next interval
-	    	try{
-	    		// Auswahl der State machine: INIT 
-	    		Vector<Integer> ssel = new Vector<Integer>();
-	    		
-	    		if (def.useCascade )
-	    			ssel.addElement(1);
-	    		if (def.useLoop )
-	    			ssel.addElement(2);
-	    		if (def.useTrans )
-	    			ssel.addElement(3);
-	    		if (ssel.size() <= 0)
-	    			rt.mode = 0;
-	    		else { // select one of the effects randomly
-	    			int sIndex = (int) (Math.random() * ssel.size()); 
-	    			rt.mode = (ssel.elementAt(sIndex)).intValue();
-	    			System.out.println("Trigger State machine! selected mode="+rt.mode);
-	    		}
-	    		//------------
-	    		
-	 	    	if (rt.mode == 1) { // cascade
-	 	    		rt.trigger_index = freq.clone(); // start the state machine
-	 	    		rt.trigger_index.getMidiNote();
-	 	    		rt.stateCnt = def.cascadeCount;
-	 	    		//rt.up = true;
-	 	    	}
-	 	    	else if (rt.mode == 2)  { // loop
-	 	    		rt.stateCnt = loop.size();
-	 	    		requestToInitLoopCopy = true;
-	 	    		loopCounter = def.loopRepeat;
-	 	    	}
-	 	    	else if (rt.mode == 3) {
-	 	    		// transposition
-	 	    		int area = Math.abs(def.transP_min) + def.transP_max;
-	 	    		int tsel= (int) (Math.random() * area + 0.5); 
-	 	    		int diff = tsel + def.transP_min;
-	 	    		System.out.println("-> Trigger Trans-diff="+diff);
-	 	    		transpositionNote += diff;
-	 	    	}
-	    	} catch (Exception ex) {}
- 	    	f_cnt = 0;
- 	    	if (rt.mode == 1)
- 	    		addLog("#### Trigger Cascade count="+def.cascadeCount+" direction="+(rt.up?"up":"down"));
- 	    	else if (rt.mode == 2)
- 	    		addLog("#### Trigger Loop effect depth="+loop.size()+" repeat="+loopCounter);
- 	    	else if (rt.mode == 2)
- 	    		addLog("#### Trigger Transposition effect trans-index="+transpositionNote);
-	    }
-	    else if (rt.stateCnt > 0) { // State Machine active !
-	    	if (rt.mode == 1) { //cascade
-	    		int no = rt.trigger_index.midiIndex;
-	    		if (rt.up) {
-	    			// select the next freq.
-	    			no = no + def.stepUp;
-	    			rt.trigger_index.setMidiNr(no); // set it
-	    			if (no > 127 || rt.trigger_index.freq > def.max_freq)
-	    				no = no - def.stepUp;
-	    			System.out.println(it+" do up:"+rt.trigger_index);
-	    		}
-	    		else {
-	    			// select next freq
-	    			no = no - def.stepDown;
-	    			rt.trigger_index.setMidiNr(no); // set it
-	    			if (no < 12 || rt.trigger_index.freq < def.min_freq)
-	    				no = no + def.stepDown;
-	    			System.out.println(it+" do down:"+rt.trigger_index);
-	    		}
-	    		rt.trigger_index.setMidiNr(no); // set it
-	    		try {
-	    			freq = rt.trigger_index.clone();
-	    			System.out.println("New step: "+freq);
-	    		} catch(Exception ex) {}
-	    		rt.stateCnt--; // count down
-		    	if (rt.stateCnt <= 0) {
-		    		rt.up = !rt.up; // change dir
-		    		System.out.println("Dir changed to :"+rt.up);
-		    	}
-	    	}
-	    	else if (rt.mode == 2) { // LOOP
-	    		// busy doing loop
-	    		try {
-	    			Note tn = loopCopy.elementAt(0);
-	    			freq = tn.note.clone(); // use the oldest element,
-	    			if (!def.permutation)
-	    				loopNote = tn.clone(); // use saved Value
-	    			loopCopy.removeElementAt(0); // remove oldest element
-	    			System.out.println("it="+it+" Insert loop note: "+freq+" loop size left:"+loopCopy.size());
-	    			if (loopCopy.size() <= 0) {
-	    				loopCounter--;
-	    				if (loopCounter > 0) { // still busy, init again
-	    					rt.stateCnt = loop.size();
-	    	 	    		loopCopy = new Vector<Note>();
-	    	 	    		loopCopy.addAll(loop);
-	    	 	    		addLog("# Repeat loop nr="+loopCounter);
-	    	 	    		System.out.println("Add new loop "+loopCopy.size());
-	    				}
-	    			}
-	    			else {
-	    				rt.stateCnt--; // count down
-	    			}
-	    		} catch (Exception ex) {
-	    			rt.stateCnt = 0;
-	    		}
-	    	}
-	    }
-	   
-	    //----------------------------------------------
+	    rückgabe werte, in class object packen addLoop(freq, loop, start, f_cnt, loopCopy, loopCounter, requestToInitLoopCopy, transpositionNote, it);
+	   s
+	    //----------------f_cnt------------------------------
 	    //System.out.println("Selected Index ="+is+" f_cnt="+f_cnt);
 	    addLog("Frequenz selected f="+freq);
 	    debugOut("This is our new selection:"+freq+" Hz", 5);
 	    //debugOut("This is our new selection:"+freq+" Hz", 55);
 	    // Mark the new individual in the RT, now update Tables
-	    rt.writeNEntrys( freq, this.def.degression, this.def.diff_freq ); // footprint
+	   doppelt?  rt.writeNEntrys( freq, this.def.degression, this.def.diff_freq ); // footprint
 	    sil.displayRT(rt);
 	    rt.getMaxx();
 	    addImageS(rt, start);
@@ -1156,6 +1035,132 @@ public class Project2 {
 	}
 	return ret;
 	
+    }
+    
+    private void addLoop(OneNote freq, Vector<Note> loop, double start, int f_cnt, 
+    		Vector<Note> loopCopy, int loopCounter, boolean requestToInitLoopCopy, 
+    		int transpositionNote, int it) {
+    	 boolean addToLoop = false;
+    	 if (rt.stateCnt <= 0) {
+ 	    	// update the loop
+ 	    	Note tn = new Note(start, 0, 0, 0, 0, 0, 0, 0, false);
+ 		    tn.note = freq;
+ 	    	if (loop.size() < def.loopDepth)
+ 	    		loop.addElement(tn);
+ 	    	else { // cache
+ 	    		loop.removeElementAt(0);
+ 	    		loop.addElement(tn);
+ 	    	}
+ 	    	//addLog("Add Loop-element at it="+it+" size now "+loop.size());
+ 	    	addToLoop = true;
+ 	    }
+ 	    
+ 	    Note loopNote  = null; 
+ 	    if (f_cnt >= def.trigger) { // Trigger the state machine, execute it at the next interval
+ 	    	try{
+ 	    		// Auswahl der State machine: INIT 
+ 	    		Vector<Integer> ssel = new Vector<Integer>();
+ 	    		
+ 	    		if (def.useCascade )
+ 	    			ssel.addElement(1);
+ 	    		if (def.useLoop )
+ 	    			ssel.addElement(2);
+ 	    		if (def.useTrans )
+ 	    			ssel.addElement(3);
+ 	    		if (ssel.size() <= 0)
+ 	    			rt.mode = 0;
+ 	    		else { // select one of the effects randomly
+ 	    			int sIndex = (int) (Math.random() * ssel.size()); 
+ 	    			rt.mode = (ssel.elementAt(sIndex)).intValue();
+ 	    			System.out.println("Trigger State machine! selected mode="+rt.mode);
+ 	    		}
+ 	    		//------------
+ 	    		
+ 	 	    	if (rt.mode == 1) { // cascade
+ 	 	    		rt.trigger_index = freq.clone(); // start the state machine
+ 	 	    		rt.trigger_index.getMidiNote();
+ 	 	    		rt.stateCnt = def.cascadeCount;
+ 	 	    		//rt.up = true;
+ 	 	    	}
+ 	 	    	else if (rt.mode == 2)  { // loop
+ 	 	    		rt.stateCnt = loop.size();
+ 	 	    		requestToInitLoopCopy = true;
+ 	 	    		loopCounter = def.loopRepeat;
+ 	 	    	}
+ 	 	    	else if (rt.mode == 3) {
+ 	 	    		// transposition
+ 	 	    		int area = Math.abs(def.transP_min) + def.transP_max;
+ 	 	    		int tsel= (int) (Math.random() * area + 0.5); 
+ 	 	    		int diff = tsel + def.transP_min;
+ 	 	    		System.out.println("-> Trigger Trans-diff="+diff);
+ 	 	    		transpositionNote += diff;
+ 	 	    	}
+ 	    	} catch (Exception ex) {}
+  	    	f_cnt = 0;
+  	    	if (rt.mode == 1)
+  	    		addLog("#### Trigger Cascade count="+def.cascadeCount+" direction="+(rt.up?"up":"down"));
+  	    	else if (rt.mode == 2)
+  	    		addLog("#### Trigger Loop effect depth="+loop.size()+" repeat="+loopCounter);
+  	    	else if (rt.mode == 2)
+  	    		addLog("#### Trigger Transposition effect trans-index="+transpositionNote);
+ 	    }
+ 	    else if (rt.stateCnt > 0) { // State Machine active !
+ 	    	if (rt.mode == 1) { //cascade
+ 	    		int no = rt.trigger_index.midiIndex;
+ 	    		if (rt.up) {
+ 	    			// select the next freq.
+ 	    			no = no + def.stepUp;
+ 	    			rt.trigger_index.setMidiNr(no); // set it
+ 	    			if (no > 127 || rt.trigger_index.freq > def.max_freq)
+ 	    				no = no - def.stepUp;
+ 	    			System.out.println(it+" do up:"+rt.trigger_index);
+ 	    		}
+ 	    		else {
+ 	    			// select next freq
+ 	    			no = no - def.stepDown;
+ 	    			rt.trigger_index.setMidiNr(no); // set it
+ 	    			if (no < 12 || rt.trigger_index.freq < def.min_freq)
+ 	    				no = no + def.stepDown;
+ 	    			System.out.println(it+" do down:"+rt.trigger_index);
+ 	    		}
+ 	    		rt.trigger_index.setMidiNr(no); // set it
+ 	    		try {
+ 	    			freq = rt.trigger_index.clone();
+ 	    			System.out.println("New step: "+freq);
+ 	    		} catch(Exception ex) {}
+ 	    		rt.stateCnt--; // count down
+ 		    	if (rt.stateCnt <= 0) {
+ 		    		rt.up = !rt.up; // change dir
+ 		    		System.out.println("Dir changed to :"+rt.up);
+ 		    	}
+ 	    	}
+ 	    	else if (rt.mode == 2) { // LOOP
+ 	    		// busy doing loop
+ 	    		try {
+ 	    			Note tn = loopCopy.elementAt(0);
+ 	    			freq = tn.note.clone(); // use the oldest element,
+ 	    			if (!def.permutation)
+ 	    				loopNote = tn.clone(); // use saved Value
+ 	    			loopCopy.removeElementAt(0); // remove oldest element
+ 	    			System.out.println("it="+it+" Insert loop note: "+freq+" loop size left:"+loopCopy.size());
+ 	    			if (loopCopy.size() <= 0) {
+ 	    				loopCounter--;
+ 	    				if (loopCounter > 0) { // still busy, init again
+ 	    					rt.stateCnt = loop.size();
+ 	    	 	    		loopCopy = new Vector<Note>();
+ 	    	 	    		loopCopy.addAll(loop);
+ 	    	 	    		addLog("# Repeat loop nr="+loopCounter);
+ 	    	 	    		System.out.println("Add new loop "+loopCopy.size());
+ 	    				}
+ 	    			}
+ 	    			else {
+ 	    				rt.stateCnt--; // count down
+ 	    			}
+ 	    		} catch (Exception ex) {
+ 	    			rt.stateCnt = 0;
+ 	    		}
+ 	    	}
+ 	    }
     }
     
     private OneNote doTransposition(OneNote nt, int transpositionNote) {
